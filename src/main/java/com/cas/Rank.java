8b74b5f8a8933a3ae1c638737c6d01f52ff0d57a
julian-perge <julianperge@gmail.com>
2021-07-22 03:41:20 -0400

    Reformatted with tabs.

--- src/main/java/com/cas/Rank.java
+++ src/main/java/com/cas/Rank.java
@@ -4,20 +4,19 @@ import lombok.Getter;
 
 @Getter
 public enum Rank {
-	HIGH_CARD(0),
-	PAIR(1),
-	TWO_PAIRS(2),
-	THREE_OF_A_KIND(3),
-	STRAIGHT(5),
-	FLUSH(6),
-	FULL_HOUSE(7),
-	FOUR_OF_A_KIND(9),
-	STRAIGHT_FLUSH(10);
+  HIGH_CARD(0),
+  PAIR(1),
+  TWO_PAIRS(2),
+  THREE_OF_A_KIND(3),
+  STRAIGHT(5),
+  FLUSH(6),
+  FULL_HOUSE(7),
+  FOUR_OF_A_KIND(9),
+  STRAIGHT_FLUSH(10);
 
-	private final int value;
+  private final int value;
 
-	Rank(final int value) {
-		this.value = value;
-	}
-
+  Rank(final int value) {
+    this.value = value;
+  }
 }
