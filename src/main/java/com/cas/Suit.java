787c5cc3798b6a42c7797d25471570ad0573fc79
julian-perge <julianperge@gmail.com>
2021-07-22 03:41:08 -0400

    Reformatted Suit enum with tabs. Changed Suit enum names to full names. Added valueOfLetter static method.

--- src/main/java/com/cas/Suit.java
+++ src/main/java/com/cas/Suit.java
@@ -1,15 +1,32 @@
 package com.cas;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 public enum Suit {
-	C("CLUBS"),
-	D("DIAMONDS"),
-	H("HEARTS"),
-	S("SPADES");
+  CLUBS("C"),
+  DIAMONDS("D"),
+  HEARTS("H"),
+  SPADES("S");
 
-	private final String suit;
+  public static final Map<String, Suit> BY_SINGLE_LETTER = new HashMap<>();
 
-	Suit(String suit) {
-		this.suit = suit.toUpperCase();
-	}
+  static {
+    for (Suit s : Suit.values()) {
+      BY_SINGLE_LETTER.put(s.getLetter(), s);
+    }
+  }
 
+  private final String letter;
+
+  Suit(final String letter) {
+    this.letter = letter.toUpperCase();
+  }
+
+  public static Suit valueOfLetter(final String letter) {
+    return BY_SINGLE_LETTER.get(letter.toUpperCase());
+  }
 }
